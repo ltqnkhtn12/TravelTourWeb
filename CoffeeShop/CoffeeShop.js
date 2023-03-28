1fb8c9e4c796e5fcd0db9f87b4b3630925758418
ltqnkhtn12 <tienvemiennam304@gmail.com>
2023-03-29 01:19:10 +0700

    modified:   CoffeeShop/CoffeeShop.js

--- CoffeeShop/CoffeeShop.js
+++ CoffeeShop/CoffeeShop.js
@@ -6,11 +6,11 @@ document.querySelector('#menu-btn').onclick = () => {
 
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
-    navbar.classList.toggle('active');
+    searchForm.classList.toggle('active');
 }
 
 
 let cartItem = document.querySelector('.cart-items-container');
 document.querySelector('#cart-btn').onclick = () => {
-    navbar.classList.toggle('active');
+    cartItem.classList.toggle('active');
 }
