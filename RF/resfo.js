2d7d1a7265b448d165a485643eb1395632692661
ltqnkhtn12 <tienvemiennam304@gmail.com>
2023-04-08 00:20:08 +0700

    new file:   RF/img/loading.gif modified:   RF/resfo.css modified:   RF/resfo.html modified:   RF/resfo.js

--- RF/resfo.js
+++ RF/resfo.js
@@ -6,9 +6,27 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a')
+
+
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec => {
+      let top = window.scrollY;
+      let height = window.offsetHeight;
+      let offset = window.offsetTop;
+      let id = sec.getAttribute('id');
+
+      if(top => offset && top < offset + height) {
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelectorAll('header .navbar a[href*='+id+']').classList.add('active');
+        })
+      }
+    })
 }
 
 document.querySelector('#search-icon').onclick = () => {
@@ -56,3 +74,13 @@ var swiper = new Swiper(".home-slider", {
       },
     }
   });
+
+  function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+
+  function fadeOut() {
+    setInterval(loader, 4100);
+  }
+
+  window.onload = fadeOut;
